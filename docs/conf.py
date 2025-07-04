9672187336e5f68cd7c027e2b40a50bd6e395ef7
Kaylee Scanlin <kscanlin03@gmail.com>
2025-07-04 15:43:49 -0500

    Added Sphinx theme in conf.py

---
+++ docs/conf.py
@@ -0,0 +1,24 @@
+# conf.py
+# this is the generic theme for the readthedocs website - can be changed if a different appearance for the site is desires.
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))  # or '.' if you're documenting a standalone project
+
+project = 'MyProject'
+author = 'Your Name'
+release = '0.1'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'myst_parser',  # only needed if you're using Markdown
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# HTML theme
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
